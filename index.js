1138fdebd591f764c391038c0e07aaa7be857aee
Tan Minh Nguyen <st.tanminh@gmail.com>
2021-10-10 01:30:55 +1100

    asd

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const path = require('path');
 const { server } = require('./service.js')
 if (process.env.NODE_ENV === "production") {
   server.use('/',
-    express.static('./client/build')
+    express.static((__dirname, 'client/build'))
   );
   server.get('*',(req,res)=>{
-    res.sendFile(path.resolve('./client/build','index.html'))
+    res.sendFile(path.join('./client/build','index.html'))
   })
   }
 const port = process.env.PORT || 9000;
-server.listen(port, () => {
-  console.log(`Proxy server listening on http://localhost:${port}`)
-})
+server.listen(port)
