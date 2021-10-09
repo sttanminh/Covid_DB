a6685080433b51a9a2702336f67cc014a95990c6
Tan Minh Nguyen <st.tanminh@gmail.com>
2021-10-10 03:19:50 +1100

    asd

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-const express = require('express')
+
+var express = require('express');
 const path = require('path');
-const { server } = require('./service.js')
-if (process.env.NODE_ENV === "production") {
-  server.use('/',
-    express.static((__dirname, 'client/build'))
-  );
-  server.get('/*',(req,res)=>{
-    res.sendFile(path.join('./client/build','index.html'))
-  })
+var app = express();
+const port = process.env.PORT || "9000";
+const cors = require('cors')
+const request = require('superagent')
+const jsdom = require("jsdom");
+// With middleware
+app.use('/', function(req, res, next){
+    
+    var options = {
+        root: path.join(__dirname)
+    };
+     
+    var fileName = 'index.html';
+    res.sendFile(path.join(__dirname, fileName), options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+            next();
+        }
+    });
+});
+ 
+app.get('/', function(req, res){
+    console.log("File Sent")
+    res.send();
+});
+ 
+
+
+
+app.use(cors())
+app.use((_, res, next) => {
+  res.header('Content-Type', 'application/json')
+  next()
+})
+
+app.get('/victoria-cases', async function(_, res) {
+  console.log(`Proxy accessed`)
+  let vicCases = []
+  let response = await callApi()
+  let htmlData = response.text
+  const dom = new jsdom.JSDOM(htmlData);
+  let table = dom.window.document.querySelector("table.DAILY-CASES");
+  for (let i = table.children[0].children.length - 1; i >= 0; i--) {
+    let eachDateObject = {}
+    eachDateObject.date = table.children[0].children[i].children[0].textContent
+    eachDateObject.cases = parseInt(table.children[0].children[i].children[1].textContent)
+    vicCases.push(eachDateObject)
   }
-const port = process.env.PORT || 9000;
-server.listen(port)
+  console.log(vicCases)
+  res.status(201)
+  res.json(vicCases)
+})
+
+function callApi() {
+  return new Promise((resolve, reject) => {
+    return request
+      .get(`https://covidlive.com.au/report/daily-cases/vic`)
+      .end((err, res) => {
+        if (!err) {
+          console.log("GET finished")
+          resolve(res)
+        }
+        else {
+          console.log("GET failed")
+          reject(err)
+        }
+      })
+  })
+}
+
+
+
+app.listen(port, function(err){
+    if (err) console.log(err);
+    console.log("app listening on PORT", port);
+});
+
+
