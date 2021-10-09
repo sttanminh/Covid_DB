2672019b1d8dd07f77457c22664848af9bd11901
Tan Minh Nguyen <st.tanminh@gmail.com>
2021-10-10 01:02:52 +1100

    asd

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === "production") {
     express.static('./client/build')
   );
   server.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'/client/public','index.html'))
+    res.sendFile(path.resolve('./client/build','index.html'))
   })
   }
 const port = process.env.PORT || 9000;
